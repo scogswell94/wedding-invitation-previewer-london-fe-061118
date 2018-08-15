5d4d3e6fa8c996e44d602d6f25156284207098e4
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-15 17:03:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", e => {
+  console.log(document.getElementById("one"))
+ document.getElementById("one").addEventListener("click", function(){
+   console.log("Clicked")
+  document.querySelector("img").setAttribute("src","https://png.pngtree.com/element_origin_min_pic/17/04/26/092f719b18ffecdf115f60326030b529.jpg") 
+});
+  
+})
+
+document.addEventListener("DOMContentLoaded", e => {
+  console.log(document.getElementById("two"))
+ document.getElementById("two").addEventListener("click", function(){
+   console.log("Clicked")
+  document.querySelector("img").setAttribute("src","https://cdn130.picsart.com/258905765034212.png?r1024x1024") 
+});
+  
+})
+
+
+document.addEventListener("DOMContentLoaded", e => {
+  console.log(document.getElementById("three"))
+ document.getElementById("three").addEventListener("click", function(){
+   console.log("Clicked")
+  document.querySelector("img").setAttribute("src","https://i.pinimg.com/originals/71/d8/28/71d828c3ca9135c1e810021366651726.png") 
+});
+  
+})
